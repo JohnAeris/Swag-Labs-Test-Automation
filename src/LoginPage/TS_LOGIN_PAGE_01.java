06214eb8f164d1e551d8135d97bed46ee3b3b049
John Aeris <barrotjohnaeris@gmail.com>
2023-04-15 23:53:13 +0800

    Added the TC_LP_00 test script

--- src/LoginPage/TS_LOGIN_PAGE_01.java
+++ src/LoginPage/TS_LOGIN_PAGE_01.java
@@ -1,10 +1,29 @@
 package LoginPage;
 
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.edge.EdgeDriver;
+
 public class TS_LOGIN_PAGE_01 {
-
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	
+	//"D:\Projects\Test Automation\Swag Labs\drivers\Edge\edgedriver_win64\msedgedriver.exe"
+	
+	@Test
+	void TC_LP_00() {
+		System.setProperty("webdriver.edge.driver", "D:\\Projects\\Test Automation\\Swag Labs\\drivers\\Edge\\edgedriver_win64\\msedgedriver.exe");
+		
+		WebDriver driver = new EdgeDriver();
+		
+		driver.get("https://www.saucedemo.com");
+		
+		String expectedURL = "https://www.saucedemo.com/";
+		String actualURL = driver.getCurrentUrl().toString();
+		System.out.println(actualURL);
+		
+		Assert.assertEquals(expectedURL, actualURL);
+		
+		driver.quit();
 	}
 
 }
