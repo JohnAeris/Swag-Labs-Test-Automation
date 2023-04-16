2a9fbbd0335cb5adfc365dd4f3381e41da78879f
John Aeris <barrotjohnaeris@gmail.com>
2023-04-16 23:04:03 +0800

    Added the initial variables

--- src/ProductsPage/TS_LEFT_MENU_NAVIGATION_03.java
+++ src/ProductsPage/TS_LEFT_MENU_NAVIGATION_03.java
@@ -1,6 +1,9 @@
 package ProductsPage;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.BeforeClass;
+import org.testng.asserts.SoftAssert;
 
 public class TS_LEFT_MENU_NAVIGATION_03 {
 
@@ -8,5 +11,10 @@ public class TS_LEFT_MENU_NAVIGATION_03 {
 	public static void beforeClass() {
 		System.setProperty("webdriver.edge.driver", "D:\\Projects\\Test Automation\\Swag Labs\\drivers\\Edge\\edgedriver_win64\\msedgedriver.exe");
 	}
+	
+	private WebDriver driver;
+	private SoftAssert softAssert;
+	private WebElement usernameField, passwordField, loginButton;
+	private WebElement menuButton;
 
 }
