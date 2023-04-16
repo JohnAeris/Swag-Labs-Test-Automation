1304c8c7aefe89a9b698abe3615e9dbb21e96534
John Aeris <barrotjohnaeris@gmail.com>
2023-04-16 22:06:47 +0800

    Added the beforeClass method

--- src/ProductsPage/TS_PRODUCTS_PAGE_01.java
+++ src/ProductsPage/TS_PRODUCTS_PAGE_01.java
@@ -1,10 +1,14 @@
 package ProductsPage;
 
+import org.testng.annotations.BeforeClass;
+
 public class TS_PRODUCTS_PAGE_01 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	@BeforeClass
+	public static void beforeClass() {
+		System.setProperty("webdriver.edge.driver", "D:\\Projects\\Test Automation\\Swag Labs\\drivers\\Edge\\edgedriver_win64\\msedgedriver.exe");
 	}
+	
+	
 
 }
