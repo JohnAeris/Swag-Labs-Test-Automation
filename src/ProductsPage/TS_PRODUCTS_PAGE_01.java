d2b9b2b4c01f7fa14c205c9228358887f052b89d
John Aeris <barrotjohnaeris@gmail.com>
2023-04-16 22:07:20 +0800

    Declared the initial variables

--- src/ProductsPage/TS_PRODUCTS_PAGE_01.java
+++ src/ProductsPage/TS_PRODUCTS_PAGE_01.java
@@ -1,6 +1,9 @@
 package ProductsPage;
 
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
 import org.testng.annotations.BeforeClass;
+import org.testng.asserts.SoftAssert;
 
 public class TS_PRODUCTS_PAGE_01 {
 
@@ -8,6 +11,10 @@ public class TS_PRODUCTS_PAGE_01 {
 	public static void beforeClass() {
 		System.setProperty("webdriver.edge.driver", "D:\\Projects\\Test Automation\\Swag Labs\\drivers\\Edge\\edgedriver_win64\\msedgedriver.exe");
 	}
+	
+	private WebDriver driver;
+	private SoftAssert softAssert;
+	private WebElement usernameField, passwordField, loginButton;
 	
 	
 
