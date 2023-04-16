929391ab2b7d70269c48b1e8c48c974273e1dea2
John Aeris <barrotjohnaeris@gmail.com>
2023-04-16 22:19:00 +0800

    Added the beforeClass method

--- src/ProductsPage/TS_HEADER_02.java
+++ src/ProductsPage/TS_HEADER_02.java
@@ -1,10 +1,26 @@
 package ProductsPage;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+import org.testng.SkipException;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeClass;
+import org.testng.annotations.BeforeMethod;
+import org.testng.asserts.SoftAssert;
+
 public class TS_HEADER_02 {
 
-	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-
+	@BeforeClass
+	public static void beforeClass() {
+		System.setProperty("webdriver.edge.driver", "D:\\Projects\\Test Automation\\Swag Labs\\drivers\\Edge\\edgedriver_win64\\msedgedriver.exe");
 	}
+	
+	
+	
+	
+	
+	
 
 }
