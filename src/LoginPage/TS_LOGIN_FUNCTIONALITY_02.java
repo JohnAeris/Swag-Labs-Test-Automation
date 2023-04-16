590de7ffc912604040da947a19ece5a1f8ca3d6f
John Aeris <barrotjohnaeris@gmail.com>
2023-04-16 16:55:46 +0800

    Added the TC_LF_02

--- src/LoginPage/TS_LOGIN_FUNCTIONALITY_02.java
+++ src/LoginPage/TS_LOGIN_FUNCTIONALITY_02.java
@@ -72,7 +72,18 @@ public class TS_LOGIN_FUNCTIONALITY_02 {
 		softAssert.assertAll();
 	}
 	
-
+	@Test // TC_LF_02: Login with incorrect username and correct password
+	void TC_LF_02() {
+		usernameField.sendKeys("standard_user123");
+		passwordField.sendKeys("secret_sauce");
+		loginButton.click();
+		
+		String expectedPage = "https://www.saucedemo.com/inventory.html";
+		String actualPage = driver.getCurrentUrl();
+		
+		softAssert.assertNotEquals(expectedPage, actualPage);
+		softAssert.assertAll();
+	}
 	
 
 	
