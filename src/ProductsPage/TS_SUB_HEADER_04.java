6a273543a4b0e2fb3c490fe48b7e5f973e12b9bc
John Aeris <barrotjohnaeris@gmail.com>
2023-04-17 22:30:27 +0800

    Declared the initial variables

--- src/ProductsPage/TS_SUB_HEADER_04.java
+++ src/ProductsPage/TS_SUB_HEADER_04.java
@@ -16,7 +16,11 @@ public class TS_SUB_HEADER_04 {
 	public static void beforeClass() {
 		System.setProperty("webdriver.edge.driver", "D:\\Projects\\Test Automation\\Swag Labs\\drivers\\Edge\\edgedriver_win64\\msedgedriver.exe");
 	}
-
+	
+	private WebDriver driver;
+	private SoftAssert softAssert;
+	private WebElement usernameField, passwordField, loginButton;
+	
 
 	
 
