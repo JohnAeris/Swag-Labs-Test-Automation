97e62cf043683dc2ce893d3917ddf75708ae0c8a
John Aeris <barrotjohnaeris@gmail.com>
2023-04-20 22:49:14 +0800

    Declared the initial variables

--- src/ProductsPage/TS_FOOTER_07.java
+++ src/ProductsPage/TS_FOOTER_07.java
@@ -17,6 +17,11 @@ public class TS_FOOTER_07 {
 		System.setProperty("webdriver.edge.driver", "D:\\Projects\\Test Automation\\Swag Labs\\drivers\\Edge\\edgedriver_win64\\msedgedriver.exe");
 	}
 	
+	private WebDriver driver;
+	private SoftAssert softAssert;
+	private WebElement usernameField, passwordField, loginButton;
+
+	
 	
 
 }
